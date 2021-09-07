79ee2cc6511d6fc0786b78b4d80612564c9bbdf3
Ellyse <85855848+ellysecarter@users.noreply.github.com>
2021-09-07 16:47:42 -0700

    completed js and cleaned up html to be shorter and make app more dynamic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,107 @@
 
-var timestamp = moment ()
+// Time sheet variable
+var eventData = [
+    {
+        hour: "9",
+        daytime: "am",
+        altHour: 9
+    },
+    {
+        hour: "10",
+        daytime: "am",
+        altHour: 10
 
-// add day and current date to top of page
-var date = new Date();
-var n = date.toDateString();
-document.getElementById('currentDay').innerHTML = n + ' ';
+    },
+    {
+        hour: "11",
+        daytime: "am",
+        altHour: 11
+    },
+    {
+        hour: "12",
+        daytime: "pm",
+        altHour: 12
+    },
+    {
+        hour: "1",
+        daytime: "pm",
+        altHour: 13
+    },
+    {
+        hour: "2",
+        daytime: "pm",
+        altHour: 14
+    },
+    {
+        hour: "3",
+        daytime: "pm",
+        altHour: 15
+    },
+    {
+        hour: "4",
+        daytime: "pm",
+        altHour: 16
+    },
+    {
+        hour: "5",
+        daytime: "pm",
+        altHour: 17
+    },
+    
+]
 
+var showTime = moment().format("dddd, MMMM Do YYYY")
+
+var currentTime = moment().format("H")
+
+$(document).ready(function () {
+    
+    $("#nowTime").text(showTime);
+    function addTimeBlock() {
+        for (var i = 0; i < eventData.length; i++) {
+            var time = eventData[i].hour + eventData[i].daytime
+            var timeBlock = $("<div>").addClass("row");
+
+            var hour = $("<div>").addClass("col-md-2 hour").text(time);
+            var description = $("<textarea>").addClass("description col-md-9 " + eventData[i].altHour).attr("id", eventData[i].altHour);
+
+            var saveBtn = $("<button>").addClass("saveBtn col-md-1");
+
+            var icon = $("<i>").addClass("fas fa-save");
+            saveBtn.append(icon)
+            timeBlock.append(hour)
+            timeBlock.append(description)
+            timeBlock.append(saveBtn)
+            
+            
+            $(saveBtn).on("click", function () {
+                var value = $(this).siblings(".description").val()
+                var time = $(this).siblings(".description").attr("id")
+                console.log(time, value)
+                localStorage.setItem(time, value)
+            })
+            
+            $(".container").append(timeBlock)
+            // change color here
+            if (eventData[i].altHour < currentTime ){
+                description.addClass("past")
+            } else if (eventData[i].altHour === +currentTime) {
+                description.addClass("present")
+            } else description.addClass("future")
+            console.log(eventData[i].altHour,currentTime)
+        }
+        
+    }
+    addTimeBlock()
+    // set data from local storage to view
+    
+    $(".9").val(localStorage.getItem("9"))
+    $(".10").val(localStorage.getItem("10"))
+    $(".11").val(localStorage.getItem("11"))
+    $(".12").val(localStorage.getItem("12"))
+    $(".13").val(localStorage.getItem("13"))
+    $(".14").val(localStorage.getItem("14"))
+    $(".15").val(localStorage.getItem("15"))
+    $(".16").val(localStorage.getItem("16"))
+    $(".17").val(localStorage.getItem("17"))
+})
--- readme.md
+++ readme.md
@@ -6,18 +6,28 @@
 
 ## Description
 
+This is a workday scheduler that contains 1 hour time blocks from 9am - 5pm.
+The time blocks of the hours that have already passed show as grey. 
+The time block of the current hour shows in red. 
+The time blocks for the future hours show as green. 
+
 
 ## Setup/Installation Requirements
-Open terminal or a similar app on your computer and change directory to Desktop. Copy https://github.com/ellysecarter/codingguru.git.  Use git clone and paste that link after it to clone the repository.  Open the folder it provides. Open the HTML folder inside. You can also go to this link to see the full site https://ellysecarter.github.io/codingguru/
+Open terminal or a similar app on your computer and change directory to Desktop. Copy https://github.com/ellysecarter/work.  Use git clone and paste that link after it to clone the repository.  Open the folder it provides. Open the HTML folder inside. You can also go to this link to see the full site https://ellysecarter.github.io/workdayassistant/ 
 
 ## Known Bugs
- 
+None known
 
 ## Contact Details
 email: ellysecarter@yahoo.com
 
-Technologies Used
-HTML CSS JavaScript
+## Technologies Used
+HTML
+CSS
+JavaScript
+Moment
+jQuery
 
 
 ![screenshot](assets/images/screenshot.png)
+
